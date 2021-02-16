99d84210e2ba36b1f36e7cdf1e2fbf603a90427a
Debashis Kar Suvra <54812722+Suvrakar@users.noreply.github.com>
2021-02-17 02:09:16 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+let curDate=new Date(2021,5 ,5 , 7);
+curDate=curDate.getHours();
+let greeting='';
+const cssStyle={};
+
+if(curDate>=1 && curDate<12){
+  greeting='Good Morning';
+  cssStyle.color='Green';
+}
+else if(curDate>=12 && curDate<19){
+  greeting='Good Afternoon';
+  cssStyle.color='Orange';
+}
+else{
+
+  greeting='Good Night';
+  cssStyle.color='Black';
+}
+
+
+ReactDOM.render(
+<h1>Hello Sir, <span style={cssStyle}>{greeting }</span></h1>,
+  document.getElementById('root')
+);
+
